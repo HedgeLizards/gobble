63c351136eb00ed4d9f35c752461ac6862b46401
troido <troido@protonmail.com>
2024-04-20 13:32:18 +0200

    player leaving and refactor

--- server/server.js
+++ server/server.js
@@ -1,57 +1,121 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
-let connections = []
 
-wss.once('listening', () => console.log(`Listening on :${wss.address().port}`));
-
-wss.on('connection', (socket) => {
-	socket.send(JSON.stringify({type: "Hello world"}));
-	connections.push(new Connection(socket));
-});
+function main() {
+	let game = new Game()
+	let server = new Serv(game, 8080);
+	game.addPlayer(new Player("Satan", new Vec2(666, 666)));
+	setInterval(() => server.update(), 50);
+}
 
 
-setInterval(update, 50);
+class Game {
 
-
-function update() {
-
-	let players = {};
-	for (let connection of connections) {
-		let player = connection.player;
-		if (!player) { continue; }
-		players[player.name] = player.view();
+	constructor(){
+		this.players = new Map();
 	}
-	console.log(players);
-	for (let connection of connections) {
-		connection.send({type: "state", players: players});
+
+	addPlayer(player) {
+		if (this.players.has(player.name)){
+			return "name " + player.name + " is already taken";
+		}
+		console.log("new player", player.name, player);
+
+		this.players.set(player.name, player);
+		return null;
+	}
+
+	updatePlayer(player) {
+		if (!this.players.has(player.name)){
+			return "unknown player " + player.name;
+		}
+		this.players.set(player.name, player);
+	}
+
+	removePlayer(name) {
+		this.players.delete(name);
+	}
+
+	view() {
+		let players = {}
+		for (let player of this.players.values()){
+			players[player.name] = player.view();
+		}
+		return {type: "state", players: players};
 	}
 }
 
-class Connection {
+class Serv {
 
-
-	constructor(socket) {
-		this.socket = socket;
-		this.socket.on("message", m => this.onMessage(m));
-		this.player = null;
+	constructor(game, port) {
+		this.game = game;
+		this.playerIds = new Map();
+		this.connections = new Map();
+		this.nextId = 1;
+		this.wss = new WebSocketServer({port: port});
+		this.wss.once('listening', () => console.log(`Listening on :${this.wss.address().port}`));
+		this.wss.on("connection", socket => {
+			let id = this.nextId++;
+			this.connections.set(id, socket);
+			socket.on("close", msg => {
+				this.connections.delete(id);
+				let playerName = this.playerIds.get(id);
+				if (playerName) {
+					this.game.removePlayer(playerName);
+				}
+			});
+			socket.on("message", msg => {
+				let data = JSON.parse(msg);
+				if (data.type === "introduction") {
+					if (this.playerIds.has(id)) {
+						send_error(socket, "Can only introduce once");
+						return;
+					}
+					let name = data.player;
+					if (!name) {
+						send_error(socket, "invalid name " + name);
+						return
+					}
+					this.playerIds.set(id, name);
+					let err = this.game.addPlayer(new Player(name, new Vec2(...data.pos)));
+					if (err) {
+						send_error(socket, err);
+					}
+				} else if (data.type === "player") {
+					let name = this.playerIds.get(id);
+					if (!name) {
+						send_error(socket, "Who the fuck are you?");
+						return
+					}
+					let player = new Player(name, new Vec2(...data.pos))
+					let err = this.game.updatePlayer(player);
+					if (err) {
+						send_error(socket, err);
+						console.log(err);
+					}
+				}
+			});
+		});
 	}
 
-	onMessage(m) {
-		let data = JSON.parse(m);
-		if (data.type === "introduction") {
-			this.player = new Player(data.player, new Vec2(...data.pos));
-		} else if (data.type === "player") {
-			this.player.pos = new Vec2(...data.pos);
+
+	update() {
+		let data = this.game.view();
+		// console.log(data.players);
+		this.broadcast(data);
+	}
+
+	broadcast(data) {
+		for (let socket of this.connections.values()) {
+			socket.send(JSON.stringify(data));
 		}
 	}
-
-	send(data) {
-		this.socket.send(JSON.stringify(data))
-	}
 }
 
-
+function send_error(socket, msg) {
+	console.warn(msg);
+	socket.send(JSON.stringify({type: "error", msg: msg}));
+}
 
 class Player {
 
@@ -147,5 +211,5 @@ class Vec2 {
 	}
 }
 
-connections.push({send: () => {}, player: new Player("God", new Vec2(100, 200))});
+main();
 
