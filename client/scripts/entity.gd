9fb2de8a5c6441e943006e8cdbe3d945c231127d
Julian Vos <julianvosje@gmail.com>
2024-04-21 11:23:43 +0200

    Synchronize interpolated aiming

--- client/scripts/entity.gd
+++ client/scripts/entity.gd
@@ -8,3 +8,8 @@ var skin
 
 func is_enemy():
 	return enemy
+
+
+func aim(towards):
+	$Weapon.rotation = towards
+	$Weapon.scale.y = -1 if towards > 0.5 * PI && towards < 1.5 * PI else 1
--- server/player.js
+++ server/player.js
@@ -2,14 +2,15 @@
 
 export class Player {
 
-	constructor(name, skin, pos) {
+	constructor(name, skin, pos, aim) {
 		this.name = name;
 		this.skin = skin;
 		this.pos = pos;
+		this.aim = aim;
 	}
 
 	view() {
-		return {type: "entityUpdated", id: this.name, skin: this.skin, pos: this.pos.arr(), isEnemy: false};
+		return {type: "entityUpdated", id: this.name, skin: this.skin, pos: this.pos.arr(), aim: this.aim, isEnemy: false};
 	}
 
 }
