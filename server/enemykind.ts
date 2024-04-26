1ea9794b1c1089364b0b519bbfa5e7a2b2eca445
troido <troido@protonmail.com>
2024-04-26 22:27:22 +0200

    also translate enemykind to typescript

--- server/enemykind.ts
+++ server/enemykind.ts
@@ -1,12 +1,23 @@
 
 
 export class EnemyKind {
+	attackCooldown: number
+	speed: number
+	skin: string
+	health: number
+	range: number
+	weapon: string
+	projectile: string
+	damage: number
 
 	constructor(args) {
 		this.attackCooldown = 1;
 		this.speed = 2;
 		Object.assign(this, args);
 	}
+
+	static Knight: EnemyKind
+	static Archer: EnemyKind
 }
 
 EnemyKind.Knight = new EnemyKind({skin: "knight", health: 10, range: 1, weapon: "Sword", projectile: "sword", damage: 5});
