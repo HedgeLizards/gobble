7044bceffd37d4b54aec145baeab716c16199c9c
Julian Vos <julianvosje@gmail.com>
2024-05-04 22:33:55 +0200

    Disable shockwave blur in web builds

--- client/scripts/shockwave.gd
+++ client/scripts/shockwave.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+@onready var color_rect_material = $ColorRect.material
+
 func _ready():
+	if OS.has_feature('web'):
+		color_rect_material.set_shader_parameter('blur_center', 0.0)
+		color_rect_material.set_shader_parameter('blur_edge', 0.0)
+	
 	var tween = create_tween().set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_OUT)
 	
-	tween.tween_method(func(value): $ColorRect.material.set_shader_parameter('progress', value), 0.0, 1.0, 0.8)
+	tween.tween_method(func(value): color_rect_material.set_shader_parameter('progress', value), 0.0, 1.0, 0.8)
 	tween.tween_callback(queue_free)
